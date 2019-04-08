6e22789f9e8fe7faa9c9898d145052f313f6c309
CFran <cfranis@ualberta.ca>
2019-04-08 11:52:25 -0600

    working test program

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,19 @@
-/*
-  Creation Date: Tuesday, 29th January 2019 
-  Original Author: Christian Francisco 
-  Contents of File: Main loop of the program to poll a vehicle over OBD
-  and transmit via bluetooth
-*/
-
-#include <sstream>
+// platformio device monitor > final_trip.txt
 
 #include <Arduino.h>
 
-#include "hardware/obd_uart.hpp"
-#include "hardware/bluetooth.hpp"
-#include "hardware/breakpoint_counter.hpp"
-#include "obd/io.hpp"
-#include "obd/profile.hpp"
-#include "debug/log.hpp"
+#include <sstream>
+#include <list>
 
-#define BT_SERIAL Serial4 // Serial4 uses pins 31 (RX) and 32 (TX)
-#define BT_BAUD 9600      // OPTIONAL: change to Serial2 and 460800
+#include <OBD2UART.h>
+
+#define BT_SERIAL Serial2 // Serial4 uses pins 31 (RX) and 32 (TX)
+#define BT_BAUD 9600      // OPTIONAL: change to Serial2 and 460800 \
                           //  for faster data transfer
 #define BT_CMD 24         // TODO: make the defines constant variables
 #define BT_STATE 25
 
-hardware::ObdUart::Pids_t pids =
+byte pids[] =
     {PID_RPM, PID_ENGINE_LOAD, PID_COOLANT_TEMP, PID_ABSOLUTE_ENGINE_LOAD,
      PID_ENGINE_OIL_TEMP, PID_ENGINE_TORQUE_PERCENTAGE, PID_ENGINE_REF_TORQUE,
      PID_INTAKE_TEMP, PID_INTAKE_MAP, PID_MAF_FLOW, PID_BAROMETRIC,
@@ -31,31 +22,66 @@ hardware::ObdUart::Pids_t pids =
      PID_CONTROL_MODULE_VOLTAGE,
      PID_FUEL_LEVEL};
 
-hardware::ObdUart obd_uart;
-hardware::Bluetooth bluetooth(BT_SERIAL, BT_CMD, BT_STATE, BT_BAUD);
-hardware::BreakpointCounter &breakpoint_counter = hardware::BreakpointCounter::get_instance();
-obd::Io io(bluetooth, obd_uart, breakpoint_counter);
+struct supported_pid
+{
+    supported_pid(byte pid, bool supported) : pid(pid), supported(supported) {}
+    byte pid;
+    bool supported;
+};
+std::list<supported_pid> supported_pids;
+
+std::stringstream output;
+COBD obd;
+char vin[64];
+// bool flag = false;
 
 void setup()
 {
-    obd_uart.begin();
-    bluetooth.begin();
-    obd::Profile profile;
-    for (const auto& pid : pids)
-        profile.add_pid(pid, 1000);
-   io.load_profile(profile);
+    delay(1000);
+    Serial.begin(9600);
+    BT_SERIAL.begin(BT_BAUD);
+    obd.begin();
+    do
+    {
+        Serial.println("DEBUG: Attempting to connect OBD");
+    } while (!obd.init());
+    obd.getVIN(vin, 64);
+    
+    for (const auto &pid : pids)
+    {
+        int result;
+        bool supported = obd.readPID(pid, result);
+        if (!supported)
+            Serial.printf("DEBUG: PID %x not supported! \n", pid);
+        supported_pids.emplace_back(pid, supported);
+    }
 }
 
 void loop()
 {
-    if (bluetooth.is_connected())
-        debug::Log::info("connected");
-    else
-        debug::Log::info("not connected");
-    hardware::ObdUart::Pids_and_values_t pid_values = obd_uart.read_pids(pids);
-    std::stringstream output;
-    for (const auto& pid_value : pid_values)
-        output << pid_value.value << ",";
-    bluetooth.transmit(output.str());
-    delay(1000);
+    Serial.println("DEBUG: Start of loop");
+    output << vin;
+    for (const auto &pid : supported_pids)
+    {
+        int result = -999; // error code
+        if (pid.supported)
+            obd.readPID(pid.pid, result);
+        output << "," << result;
+    }
+    BT_SERIAL.println(output.str().c_str());
+    Serial.println(output.str().c_str());
+    output.str("");
+    delay(300);
+
+    // if (flag)
+    // {
+    //     BT_SERIAL.println("1,1,1,1,1,1,1");
+    //     flag = false;
+    // }
+    // else
+    // {
+    //     BT_SERIAL.println("0,0,0,0,0,0,0,0");
+    //     flag = true;
+    // }
+    // delay(1000);
 }
