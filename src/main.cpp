4ba404bbef43535681c76e8de62b1c890eaed879
Brohammers <31675499+Brohammers@users.noreply.github.com>
2019-04-10 12:16:12 -0600

    added header block

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+/*
+  Creation Date: Tuesday, 29th January 2019 
+  Original Author: Christian Francisco 
+  Contents of File: Main loop of the program to poll a vehicle over OBD
+  and transmit via bluetooth
+*/
 // platformio device monitor > final_trip.txt
 
 #include <Arduino.h>
